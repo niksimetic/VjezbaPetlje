1c1d25afab64374b4920100a4a452002fe4419bf
niksimetic <55134764+niksimetic@users.noreply.github.com>
2019-10-24 11:34:38 +0200

    Add files via upload

---
+++ nikSimeticString2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikSimeticString2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Unesi jednu rijec: ");
+            text = Console.ReadLine();
+            for (int i = text.Length; i >0; i--)
+            {
+                Console.WriteLine(text.Substring(0, i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
