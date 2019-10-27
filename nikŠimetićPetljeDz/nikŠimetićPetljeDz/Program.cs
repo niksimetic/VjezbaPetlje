0497204d1781386f4c1fbf5d0213638bd7e297f3
niksimetic <55134764+niksimetic@users.noreply.github.com>
2019-10-27 22:37:19 +0100

    Add files via upload

---
+++ nikŠimetićPetljeDz/nikŠimetićPetljeDz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikŠimetićPetljeDz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocjena, brOc = 0, prosjek = 0;
+            Console.WriteLine("Upisite ocjene: ");
+       
+        nazad:
+
+            ocjena = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+
+                if (ocjena == 1)
+                {
+                    Console.WriteLine("Prosjek je 1 ");
+                    goto dalje;
+                }
+                
+                if (ocjena < 1 || ocjena > 5)
+                {
+                    Console.WriteLine("Nije ocjena. Probajte ponovno");
+                    
+                    goto nazad;
+
+                }
+                prosjek = prosjek + ocjena;
+                brOc++;
+             
+
+            prosjek = prosjek / brOca;
+            Console.WriteLine("Prosjek je: " + prosjek);
+
+        dalje:
+
+            Console.ReadKey();
+        }
+    }
+}
